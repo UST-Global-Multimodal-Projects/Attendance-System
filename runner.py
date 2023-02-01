10f4b096f8a05f0f088e434de362ab5867ab99b6
Aditya Gitte <adityagitte@Adityas-MacBook-Pro.local>
2023-02-01 16:57:47 +0530

    handled placeholder inputs

--- runner.py
+++ runner.py
@@ -16,6 +16,7 @@ face_reidentification_model_path=os.environ['face_reidentification_model_path']
 landmark_regression_model_path=os.environ['landmark_regression_model_path']
 face_database_path=os.environ['face_database_path']
 face_gallary_crop_flag=os.environ['face_gallary_crop_flag']
+debug_flag=os.environ['debug_flag']
 
 
 #clear the input folder so that all the cropped images from the previous execution are deleted
@@ -76,6 +77,8 @@ print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ face recogni
 files = os.listdir(input_directory_path)
 counter=0
 for file in files:
+    if file==".placeholder":
+        continue
     counter=counter+1
     file_path = os.path.abspath(os.path.join(input_directory_path, file))
     cmd = f'python face_recognition.py -i "{file_path}" -fg "{face_database_path}" -m_fd "{face_detection_model_path}" -m_lm "{landmark_regression_model_path}" -m_reid "{face_reidentification_model_path}"'
@@ -91,6 +94,22 @@ print("--------------------------------------------------------Clearing the coor
 with open(coordinates_file_path, 'w') as coordinates_file:
     coordinates_file.truncate()
 
+
+#clearing the Input_Images directory for the next image if dubug flag is set to 0
+# if debug_flag==0:
+#     print("-----------------------------clearing the inputs directory---------------------------------")
+#     dir_path = input_directory_path
+
+#     for filename in os.listdir(dir_path):
+#         file_path = os.path.join(dir_path, filename)
+#         try:
+#             if os.path.isfile(file_path) or os.path.islink(file_path):
+#                 os.unlink(file_path)
+#             elif os.path.isdir(file_path):
+#                 shutil.rmtree(file_path)
+#         except Exception as e:
+#             print('Failed to delete %s. Reason: %s' % (file_path, e))
+#     print("-----------------------------cleared the inputs directory---------------------------------")
 
 
 
